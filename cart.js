5f25f00a3dd2f2838df486410b235e4fab0cac28
Fredrik Ljungqvist <fljungqvist@gmail.com>
2020-10-28 15:55:43 +0100

    Now i works to print stuff from localStorge in cart.html

--- cart.js
+++ cart.js
@@ -1,9 +1,13 @@
 // JavaScript for cart
+function initSite() {
+    printCart();
+}
+
 function printCart() {
     let list = getCartList()
    for (let i = 0; i < list.length; i++) {
-       const element = list[i];
-       
+       let product = list[i];
+       document.getElementById("main").innerHTML += product.title + " " + product.description + " " + product.price;
    }
     
     
