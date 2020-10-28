4c9762d0855cc856887026eda328ea980891ce3c
Fredrik Ljungqvist <fljungqvist@gmail.com>
2020-10-28 15:24:47 +0100

    starting to make progress

--- cart.js
+++ cart.js
@@ -1,1 +1,19 @@
 // JavaScript for cart
+function printCart() {
+    let list = getCartList()
+   for (let i = 0; i < list.length; i++) {
+       const element = list[i];
+       
+   }
+    
+    
+}
+function getCartList() {
+    let cartList = localStorage.getItem("cartList")
+    if (cartList) {
+        cartList = JSON.parse(cartList)
+    } else {
+        cartList = []
+    }
+    return cartList
+}
