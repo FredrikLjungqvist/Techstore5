4a57768912dbcd677432093de64d67b531708cbc
Fredrik Ljungqvist <fljungqvist@gmail.com>
2020-11-17 18:03:07 +0100

    fixat mera

--- loggedin.js
+++ loggedin.js
@@ -1,7 +1,7 @@
-window.addEventListener("load", initSite)
-
-function initSite() {
-   /* let log = {
+function initLoggedIn() {
+   document.getElementById("main").innerHTML=""
+   document.getElementById("cart").innerHTML=""
+    /* let log = {
     username: "Fredrik",
     password: "123",
     orders: []
@@ -44,14 +44,14 @@ function getOrders() {
         date.id = "dateId"
         date.innerHTML = order.date + " Totalpris " + totalPrice()
         
-        document.getElementById("test").appendChild(date)
+        document.getElementById("loggedIn").appendChild(date)
         console.log(order.date)
         order.products.forEach((product) => {
             
             let productName = document.createElement("h3")
             productName.innerHTML =  product.title
             productName.id = "productName"
-            document.getElementById("test").appendChild(productName)
+            document.getElementById("loggedIn").appendChild(productName)
             console.log(productName)
             
 
